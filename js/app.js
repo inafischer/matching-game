ee05202bff1826a65ce9c5854d5cad0913d3f345
Ina-Fischer <ina.fischer@booking.com>
2018-08-28 22:54:42 +0200

    final updates made to meet rubric

--- js/app.js
+++ js/app.js
@@ -1,13 +1,59 @@
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+// Global variables
+
+const openCards = [];
+let timer;
+
+// Entry point called when page is loaded
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  const cards = document.querySelector('.deck');
+  shuffle(cards);
+  cards.addEventListener('click', cardListener);
+  const refresh = document.querySelector('.restart');
+  timer = startTimer();
+  refresh.addEventListener('click', refreshListener);
+});
+
+// Event listeners
+
+function refreshListener(event) {
+  const cards = document.querySelector('.deck');
+  shuffle(cards);
+  for (let i = 0; i < cards.children.length; i++) {
+    const card = cards.children[i];
+    resetCard(card);
+  }
+  resetMoves();
+  resetRating();
+  clearInterval(timer);
+  startTimer();
+}
+
+function cardListener(event) {
+  const element = event.target;
+  if(element.classList.contains('card')
+    && !element.classList.contains('match')
+    && !element.classList.contains('open') ) {
+    displaySymbol(element);
+    addOpenCard(element);
+  }
+  else {
+    console.log('clicked the deck');
+  }
+}
+
+// Gameplay functions
+
+function startTimer(){
+    let sec = 0;
+    document.getElementById('gameTimerDisplay').innerHTML = '0 seconds';
+    const timer = setInterval(function(){
+        const time = sec + (sec == 1 ?' second':' seconds');
+        document.getElementById('gameTimerDisplay').innerHTML = time;
+        sec++;
+    }, 1000);
+    return timer;
+}
 
 function displaySymbol(card) {
   card.classList.add('open');
@@ -25,21 +71,45 @@ function lockOpenCard(card) {
   card.classList.add('match');
 }
 
+function addOpenCard(card) {
+  openCards.push(card);
+
+  if (openCards.length > 1) {
+      const firstCard = openCards.pop();
+      const secondCard = openCards.pop();
+      if (getIcon(firstCard) == getIcon(secondCard)) {
+        lockOpenCard(firstCard);
+        lockOpenCard(secondCard);
+      } else {
+        setTimeout(function() {
+          resetCard(firstCard);
+          resetCard(secondCard);
+        }, 500);
+      }
+      increaseMoves();
+      checkWinner();
+      lowerStarRating();
+  }
+}
+
+function getIcon(card) {
+  return card.children[0].classList[1];
+}
+
+// Shuffle function from https://stackoverflow.com/a/11972692
+function shuffle(cards) {
+  for (let i = cards.children.length; i >= 0; i--) {
+     cards.appendChild(cards.children[Math.random() * i | 0]);
+  }
+}
+
+//scoring functions
+
 function increaseMoves() {
   const moves = document.querySelector('.moves');
   moves.textContent = parseInt(moves.textContent) + 1;
 }
 
-function checkWinner() {
-  const winner = document.querySelectorAll('.match');
-  const cards = document.querySelectorAll('.card');
-
-  if (winner.length === cards.length) {
-    alert("You're a winning weenie!");
-  }
-}
-
-// Star rating
 function lowerStarRating () {
   const rating = document.querySelector('.stars');
   const moves = parseInt(document.querySelector('.moves').textContent);
@@ -57,72 +127,34 @@ function resetRating() {
   }
 }
 
-const openCards = [];
-
-function addOpenCard(card) {
-  openCards.push(card);
-
-  if (openCards.length > 1) {
-      var firstCard = openCards.pop();
-      var secondCard = openCards.pop();
-      if (getIcon(firstCard) == getIcon(secondCard)) {
-        lockOpenCard(firstCard);
-        lockOpenCard(secondCard);
-      } else {
-        resetCard(firstCard);
-        resetCard(secondCard);
-      }
-      increaseMoves();
-      checkWinner();
-      lowerStarRating();
-  }
-}
-
-function getIcon(card) {
-  return card.children[0].classList[1];
-}
-
-function cardListener(event) {
-  var element = event.target;
-  if(element.classList.contains('card')
-    && !element.classList.contains('match')
-    && !element.classList.contains('open') ) {
-    displaySymbol(element);
-    addOpenCard(element);
-  }
-  else {
-    console.log('clicked the deck');
-  }
-}
-
-// Shuffle function from https://stackoverflow.com/a/11972692
-function shuffle(cards) {
-  for (var i = cards.children.length; i >= 0; i--) {
-     cards.appendChild(cards.children[Math.random() * i | 0]);
-  }
-}
- // entry point
-document.addEventListener("DOMContentLoaded", function(event) {
-  const cards = document.querySelector('.deck');
-  shuffle(cards);
-  cards.addEventListener('click', cardListener);
-  const refresh = document.querySelector('.restart');
-  refresh.addEventListener('click', refreshListener);
-});
-
 function resetMoves() {
   const moves = document.querySelector('.moves');
   moves.textContent = 0;
 }
 
-// Add refresh
-function refreshListener(event) {
-  const cards = document.querySelector('.deck');
-  shuffle(cards);
-  for (let i = 0; i < cards.children.length; i++) {
-    const card = cards.children[i];
-    resetCard(card);
+//winning functions
+
+function checkWinner() {
+  const winner = document.querySelectorAll('.match');
+  const cards = document.querySelectorAll('.card');
+
+  if (winner.length === cards.length) {
+    clearInterval(timer);
+    const star = getStars();
+    const time = getTime();
+    const newGame = confirm("You win! Your star rating was " + star + " " + ((star == 1) ? "star":"stars") +
+      " and it took you " + time +
+      "! Would you like to play another game?");
+    if(newGame) {
+      refreshListener();
+    }
   }
-  resetMoves();
-  resetRating();
+}
+
+function getStars() {
+  return document.querySelector('.stars').children.length;
+}
+
+function getTime() {
+  return document.getElementById('gameTimerDisplay').textContent;
 }
